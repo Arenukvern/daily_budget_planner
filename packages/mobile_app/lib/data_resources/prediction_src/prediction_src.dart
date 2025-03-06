30a12b73c2d2b41b5ac8280081d221b086ecde85
Arenukvern <anton@xsoulspace.dev>
2025-03-06 22:07:34 +0300

    wip: resources

--- packages/mobile_app/lib/data_resources/prediction_src/prediction_src.dart
+++ packages/mobile_app/lib/data_resources/prediction_src/prediction_src.dart
@@ -1,4 +1,7 @@
+export 'budgets.src.dart';
 export 'daily_budget.src.dart';
 export 'one_time_sum.src.dart';
 export 'prediction_config.src.dart';
+export 'tasks.src.dart';
+export 'tasks_transactions.src.dart';
 export 'total_sum.src.dart';
--- packages/mobile_app/lib/data_states/ui_prediction_notifier.dart
+++ packages/mobile_app/lib/data_states/ui_prediction_notifier.dart
@@ -2,22 +2,4 @@ import 'package:mobile_app/common_imports.dart';
 
 class UiPredictionNotifier with HasLocalApis, HasNotifiers, HasResources {
   UiPredictionNotifier();
-
-  Future<void> onLoad() async {
-    _recalculateSums();
-  }
-
-  void _recalculateSums() {
-    _recalculateTotalExpensesSum();
-    _recalculateTotalIncomesSum();
-  }
-
-  // TODO(arenukvern): make dependent from period
-  double getExpensePredictionFor(final DateTime date) => 0;
-
-  Future<void> upsertIncome(final Transaction income) async {
-    await transactionsLocalApi.upsertTransaction(income);
-
-    _recalculateTotalIncomesSum();
-  }
 }
