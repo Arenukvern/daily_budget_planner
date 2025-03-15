090fc089e76d9ee14ce766d9ff2e7dc6ee846c70
Arenukvern <anton@xsoulspace.dev>
2025-03-15 23:18:03 +0300

    fix: scheduled transactions

--- packages/mobile_app/lib/data_local_api/db_apis/scheduled_transaction_local_api_sembast.dart
+++ packages/mobile_app/lib/data_local_api/db_apis/scheduled_transaction_local_api_sembast.dart
@@ -9,82 +9,6 @@ final class SembastScheduledTransactionsLocalApi extends ComplexLocalApi {
   SembastScheduledTransactionsLocalApi(this._db);
   final SembastDb _db;
 
-  Future<void> delete(final TransactionId id) async {
-    await _db.scheduledTransactions.record(id).delete(_db.db);
-  }
-
-  Future<ScheduledTransaction?> get(final TransactionId id) async {
-    final record = await _db.scheduledTransactions.record(id).get(_db.db);
-    if (record == null) return null;
-    return ScheduledTransactionSembastCollection.fromMap(record).item;
-  }
-
-  Future<List<ScheduledTransaction>> getAll() async {
-    final records = await _db.scheduledTransactions.find(_db.db);
-    return records
-        .map(
-          (final record) =>
-              ScheduledTransactionSembastCollection.fromMap(record.value).item,
-        )
-        .toList();
-  }
-
-  Future<List<ScheduledTransaction>> getAllByTaskId({
-    required final TaskId taskId,
-    required final DateTime startedAt,
-    required final DateTime endedAt,
-  }) async {
-    final finder = Finder(
-      filter: Filter.and([
-        Filter.equals('taskId', taskId.value),
-        Filter.lessThan('schedule.startedAt', endedAt.toIso8601String()),
-        Filter.or([
-          Filter.isNull('schedule.endedAt'),
-          Filter.greaterThan('schedule.endedAt', startedAt.toIso8601String()),
-        ]),
-      ]),
-      sortOrders: [SortOrder('id', false)],
-    );
-    final records = await _db.scheduledTransactions.find(
-      _db.db,
-      finder: finder,
-    );
-    return records
-        .map(
-          (final record) =>
-              ScheduledTransactionSembastCollection.fromMap(record.value).item,
-        )
-        .toList();
-  }
-
-  Future<void> save(final ScheduledTransaction scheduledTransaction) async {
-    final collection = ScheduledTransactionSembastCollection.fromDomain(
-      scheduledTransaction,
-    );
-    await _db.scheduledTransactions
-        .record(scheduledTransaction.transactionId)
-        .put(_db.db, collection.toMap());
-  }
-
-  Future<void> saveAll(
-    final List<ScheduledTransaction> scheduledTransactions,
-  ) async {
-    await _db.db.transaction((final txn) async {
-      for (final scheduledTransaction in scheduledTransactions) {
-        final collection = ScheduledTransactionSembastCollection.fromDomain(
-          scheduledTransaction,
-        );
-        await _db.scheduledTransactions
-            .record(scheduledTransaction.transactionId)
-            .put(txn, collection.toMap());
-      }
-    });
-  }
-
-  Future<void> deleteAll() async {
-    await _db.scheduledTransactions.delete(_db.db);
-  }
-
   Future<void> upsertScheduledTransaction(
     final ScheduledTransaction scheduledTransaction,
   ) async {
@@ -100,6 +24,104 @@ final class SembastScheduledTransactionsLocalApi extends ComplexLocalApi {
     } catch (e, s) {
       throw LocalApiException(
         message: 'Failed to create scheduled transaction',
+        error: e,
+        stackTrace: s,
+      );
+    }
+  }
+
+  Future<void> deleteScheduledTransaction(final TransactionId id) async {
+    try {
+      await _db.scheduledTransactions.record(id).delete(_db.db);
+    } catch (e, s) {
+      throw LocalApiException(
+        message: 'Failed to delete scheduled transaction',
+        error: e,
+        stackTrace: s,
+      );
+    }
+  }
+
+  Future<ScheduledTransaction> getScheduledTransaction(
+    final TransactionId id,
+  ) async {
+    try {
+      final record = await _db.scheduledTransactions.record(id).get(_db.db);
+      if (record == null) return ScheduledTransaction.empty;
+      return ScheduledTransactionSembastCollection.fromMap(record).item;
+    } catch (e, s) {
+      throw LocalApiException(
+        message: 'Failed to get scheduled transaction',
+        error: e,
+        stackTrace: s,
+      );
+    }
+  }
+
+  Future<List<ScheduledTransaction>> getAllScheduledTransactions() async {
+    try {
+      final records = await _db.scheduledTransactions.find(_db.db);
+      return records
+          .map(
+            (final record) =>
+                ScheduledTransactionSembastCollection.fromMap(
+                  record.value,
+                ).item,
+          )
+          .toList();
+    } catch (e, s) {
+      throw LocalApiException(
+        message: 'Failed to get all scheduled transactions',
+        error: e,
+        stackTrace: s,
+      );
+    }
+  }
+
+  Future<List<ScheduledTransaction>> getScheduledTransactionsByTaskId({
+    required final TaskId taskId,
+    required final DateTime startedAt,
+    required final DateTime endedAt,
+  }) async {
+    try {
+      final finder = Finder(
+        filter: Filter.and([
+          Filter.equals('taskId', taskId.value),
+          Filter.lessThan('schedule.startedAt', endedAt.toIso8601String()),
+          Filter.or([
+            Filter.isNull('schedule.endedAt'),
+            Filter.greaterThan('schedule.endedAt', startedAt.toIso8601String()),
+          ]),
+        ]),
+        sortOrders: [SortOrder('id', false)],
+      );
+      final records = await _db.scheduledTransactions.find(
+        _db.db,
+        finder: finder,
+      );
+      return records
+          .map(
+            (final record) =>
+                ScheduledTransactionSembastCollection.fromMap(
+                  record.value,
+                ).item,
+          )
+          .toList();
+    } catch (e, s) {
+      throw LocalApiException(
+        message: 'Failed to get scheduled transactions by task ID',
+        error: e,
+        stackTrace: s,
+      );
+    }
+  }
+
+  Future<void> deleteAllScheduledTransactions() async {
+    try {
+      await _db.scheduledTransactions.delete(_db.db);
+    } catch (e, s) {
+      throw LocalApiException(
+        message: 'Failed to delete all transactions',
         error: e,
         stackTrace: s,
       );
