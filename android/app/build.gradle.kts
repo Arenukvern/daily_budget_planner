0c40d5dbbf00a1d5b4b9a6429decfb407c7d620f
arenukvern <anton@xsoulspace.dev>
2025-10-29 16:39:54 +0300

    wip: fix android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,9 +6,8 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
-    id("com.google.gms.google-services")
-    id("com.google.firebase.crashlytics")
-
+    // id("com.google.firebase.crashlytics")
+    
 }
 val keystoreProperties = Properties()
 val keystorePropertiesFile = rootProject.file("key.properties")
